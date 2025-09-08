78ab3c1b86cf8c33c134d461a9d36c70f83abe53
심은수 <seunsoo102@gmail.com>
2025-09-08 13:45:16 +0900

    [Fix] 로그인 화면 하단 네브바 숨김, ui 개선
    - 하단 네브바 숨김
    - 회원가입 버튼 베이스라인 정렬

--- app/src/main/java/com/ssafy/a602/common/navigation/NavGraph.kt
+++ app/src/main/java/com/ssafy/a602/common/navigation/NavGraph.kt
@@ -1,35 +1,21 @@
-package com.ssafy.a602.common.navigation  // 패키지 경로(모듈/폴더 구조 상의 네임스페이스)
+package com.ssafy.a602.common.navigation
 
-import androidx.compose.foundation.layout.Box          // 레이아웃 컨테이너(겹쳐 배치/정렬에 유용)
-import androidx.compose.foundation.layout.fillMaxSize // 부모 사이즈를 가득 채우는 Modifier
-import androidx.compose.material3.Text                // 글자 표시용 컴포저블
-import androidx.compose.runtime.Composable            // 컴포저블 함수임을 표시하는 어노테이션
-import androidx.compose.ui.Alignment                 // Box 등에서 자식 정렬할 때 사용
-import androidx.compose.ui.Modifier                  // 컴포저블에 체이닝으로 속성 부여(크기/패딩 등)
-import androidx.compose.ui.text.font.FontWeight      // 글자 두께(굵기)
-import androidx.compose.ui.unit.sp                   // 글자 크기 단위
-import androidx.navigation.NavHostController         // 네비게이션을 실제로 조종하는 컨트롤러
-import androidx.navigation.compose.NavHost           // 라우트 그래프를 담는 컨테이너
-import androidx.navigation.compose.composable        // 특정 라우트에 컴포저블 화면을 연결
-import com.ssafy.a602.home.HomeScreen                // 홈 화면(별도 파일에서 구현)
-import com.ssafy.a602.game.GameScreen                // 게임 화면(별도 파일에서 구현)
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.ssafy.a602.home.HomeScreen
+import com.ssafy.a602.game.GameScreen
 import com.ssafy.a602.learning.LearningMainPage
 import com.ssafy.a602.learning.Total_RoadMap
 import com.ssafy.a602.login.LoginScreen
-
-import androidx.compose.foundation.layout.Box          // 레이아웃 컨테이너(겹쳐 배치/정렬에 유용)
-import androidx.compose.foundation.layout.fillMaxSize // 부모 사이즈를 가득 채우는 Modifier
-import androidx.compose.material3.Text                // 글자 표시용 컴포저블
-import androidx.compose.runtime.Composable            // 컴포저블 함수임을 표시하는 어노테이션
-import androidx.compose.ui.Alignment                 // Box 등에서 자식 정렬할 때 사용
-import androidx.compose.ui.Modifier                  // 컴포저블에 체이닝으로 속성 부여(크기/패딩 등)
-import androidx.compose.ui.text.font.FontWeight      // 글자 두께(굵기)
-import androidx.compose.ui.unit.sp                   // 글자 크기 단위
-import androidx.navigation.NavHostController         // 네비게이션을 실제로 조종하는 컨트롤러
-import androidx.navigation.compose.NavHost           // 라우트 그래프를 담는 컨테이너
-import androidx.navigation.compose.composable        // 특정 라우트에 컴포저블 화면을 연결
-import com.ssafy.a602.home.HomeScreen                // 홈 화면(별도 파일에서 구현)
-import com.ssafy.a602.game.GameScreen                // 게임 화면(별도 파일에서 구현)
 @Composable
 fun NavGraph(
     navController: NavHostController, // 화면 전환을 수행하는 컨트롤러(상위에서 rememberNavController()로 생성)

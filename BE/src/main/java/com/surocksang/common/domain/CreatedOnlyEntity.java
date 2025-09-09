f12027870b11204eda63ad0097b7b3c2cc5dc68a
33jyu33 <jy04169@gmail.com>
2025-09-09 17:34:04 +0900

    [Feat] 데이터 생성 일시 Entity

---
+++ BE/src/main/java/com/surocksang/common/domain/CreatedOnlyEntity.java
@@ -0,0 +1,17 @@
+package com.surocksang.common.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class CreatedOnlyEntity {
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+}

55877b63759768385f1508d09de31cdd5c805c1f
33jyu33 <jy04169@gmail.com>
2025-09-10 16:53:59 +0900

    [Feat] RefreshToken Entity, Repository

---
+++ BE/src/main/java/com/surocksang/auth/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.surocksang.auth;
+
+import com.surocksang.auth.domain.RefreshToken;
+import com.surocksang.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByUserAndIssuedIpAndIssuedUserAgent(User user, String issuedIp, String issuedUserAgent);
+}
+

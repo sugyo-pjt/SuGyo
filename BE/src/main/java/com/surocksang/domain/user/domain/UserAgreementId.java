5dd5d739ef901f343becb7083b9440865e1af8df
33jyu33 <jy04169@gmail.com>
2025-09-10 10:26:49 +0900

    [Feat] 회원 관련 Entity
    UserAgreementId: UserAgreement가 조합키라서 Id class 생성

---
+++ BE/src/main/java/com/surocksang/domain/user/domain/UserAgreementId.java
@@ -0,0 +1,19 @@
+package com.surocksang.domain.user.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
+public class UserAgreementId implements Serializable {
+    private Long userId;
+    private Long termId;
+}

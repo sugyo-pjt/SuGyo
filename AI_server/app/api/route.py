eb59ffdf05e4be7ca81080db4c8a8e7c16821976
Wangjooyoung <happyboyjoo@naver.com>
2025-09-10 16:39:44 +0900

    [Feat] 크로마 db 생성 및 검색 예시 구현
    Former-commit-id: e914b4038809d290917e3e6dd1d8063208726637

--- AI_server/app/api/route.py
+++ AI_server/app/api/route.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
+from fastapi.responses import JSONResponse, PlainTextResponse
+from AI_server.app.models.schemas import LandmarkInput, LandmarkOutput, ChatBotOutput, ChatbotInput, Motion
+
 
 router = APIRouter()
+
+@router.get("/health", response_class=PlainTextResponse)
+async def health():
+    return "ok"
+
+@router.get("/sing")
+async def translate(input_data = LandmarkInput):
+    pass
+
+@router.get("/chatbot")
+async def translate(input_data = ChatbotInput):
+    pass
+

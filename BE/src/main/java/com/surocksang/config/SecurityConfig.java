96d6973b160185218ee4acdbb26d1c25962d30ba
33jyu33 <jy04169@gmail.com>
2025-09-10 13:41:36 +0900

    [Feat] Security 로그인 제거

--- BE/src/main/java/com/surocksang/config/SecurityConfig.java
+++ BE/src/main/java/com/surocksang/config/SecurityConfig.java
@@ -3,14 +3,35 @@ package com.surocksang.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http.sessionManagement((session) ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {

726f05ebae33e60de6b3eb394f666a298e130fd9
왕주영 노트북 <happyboyjoo@gmail.com>
2025-09-11 23:43:37 +0900

    [Feat] 학습 데이터 전처리 및 학습 코드 일부 수정
    Former-commit-id: db57a615f29c12d889fd22f69b6b2e954fd9e39d

--- AI_train/training/config.py
+++ AI_train/training/config.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 class ModelConfig:
     SEQUENCE_LENGTH = 60 # 단어 길이에 맞춰 60프레임으로 조정
-    FEATURE_DIM = 225    # MediaPipe landmark 차원(pose:99 + hands:126 = 225), 다리 빼면 조정해야 할 수도
+    FEATURE_DIM = 234    # MediaPipe landmark 차원(얼굴 25*3 + 상체 11*3 + hands:126 = 234), 다리 빼고 조정 함.
     BUCKETS = (30, 45, 60, 75, 90)  # 버킷 기반 시퀀스 길이 정규화용
     NUM_CLASSES = 500    # AI-Hub 수어단어 데이터 기준 -> 이건 우리가 학습하려는 단어 개수로 고치면 됨  
     BATCH_SIZE = 32      # L40S 48GB GPU 최적화
-    EPOCHS = 100        # L40S 48GB GPU 최적화
+    EPOCHS = 100        # L40S 48GB GPU 최적화, 상황 보고 줄일 수 있음.
     LEARNING_RATE = 1e-3 # L40S 48GB GPU 최적화
     DROPOUT_RATE = 0.2   # L40S 48GB GPU 최적화
     MHA_HEADS = 8        # L40S 48GB GPU 최적화

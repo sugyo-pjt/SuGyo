380bab69ecb9eb95547b86f75de27c9a061611b8
심은수 <seunsoo102@gmail.com>
2025-09-15 12:20:30 +0900

    [Fix] CameraX 실험적 API 어노테이션을 @OptIn으로 변경
    - @ExperimentalMirrorMode를 @OptIn(ExperimentalMirrorMode::class)로 교체하여 컴파일 경고 해결

--- app/src/main/java/com/ssafy/a602/game/preparation/GamePreparationScreen.kt
+++ app/src/main/java/com/ssafy/a602/game/preparation/GamePreparationScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import kotlin.OptIn
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -55,7 +56,7 @@ import com.ssafy.a602.game.preparation.GamePreparationState
 import com.ssafy.a602.game.preparation.ResourceLoadingState
 import com.ssafy.a602.game.GameTheme
 
-@ExperimentalMirrorMode
+@OptIn(ExperimentalMirrorMode::class)
 @Composable
 fun GamePreparationScreen(
     song: SongItem,
@@ -541,7 +542,7 @@ private fun ErrorContent(
     }
 }
 
-@ExperimentalMirrorMode
+@OptIn(ExperimentalMirrorMode::class)
 @Preview(showBackground = true, widthDp = 360, heightDp = 800, backgroundColor = 0xFF0D1118)
 @Composable
 private fun GamePreparationScreenPreview() {

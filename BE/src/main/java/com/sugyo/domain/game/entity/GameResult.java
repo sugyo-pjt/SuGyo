af2ffabd2b22eccd192cd1b592cf1ff09c7cb58b
33jyu33 <jy04169@gmail.com>
2025-09-24 17:02:15 +0900

    be/S13P21A602-357 게임 결과 entity 수정
    Former-commit-id: 4ff08443e3a4708db0fd1dbc7dc1239e91c9da4f

---
+++ BE/src/main/java/com/sugyo/domain/game/entity/GameResult.java
@@ -0,0 +1,57 @@
+package com.sugyo.domain.game.entity;
+
+import com.sugyo.common.domain.BaseTimeEntity;
+import com.sugyo.domain.user.domain.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class GameResult extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "music_id", nullable = false)
+    private Music music;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Integer score;
+
+    public static GameResult create(User user, Music music, int score) {
+        return GameResult.builder()
+                .music(music)
+                .user(user)
+                .score(score)
+                .build();
+    }
+
+    public boolean updateScoreIfHigher(int newScore) {
+        if (this.score < newScore) {
+            this.score = newScore;
+            return true;
+        }
+        return false;
+    }
+}
+

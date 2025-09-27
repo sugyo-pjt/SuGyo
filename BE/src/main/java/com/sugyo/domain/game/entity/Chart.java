1e3f9c5708a93905744136e982deb678c5660bf2
33jyu33 <jy04169@gmail.com>
2025-09-27 21:23:18 +0900

    be/S13P21A602-392 병합 과정 import 에러 해결
    Former-commit-id: bdc375f6c9fd9ed2dd49aa8a519a0a64e856738c

--- BE/src/main/java/com/sugyo/domain/game/entity/Chart.java
+++ BE/src/main/java/com/sugyo/domain/game/entity/Chart.java
@@ -1,6 +1,16 @@
 package com.sugyo.domain.game.entity;
 
-import jakarta.persistence.*;
+import com.sugyo.domain.music.domain.Music;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,17 +24,17 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class Chart {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private Integer sequence;
-    
+
     @Column(nullable = false, length = 50)
     private String lyrics;
-    
+
     @Column(nullable = false)
     private LocalTime startedAt;
 
